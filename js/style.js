58960288831e319b12eb64b7aa7753555fb27aed
ura148 <yuki.ura10@gmail.com>
2020-05-22 11:45:59 +0900

    fix：sp版におけるhover効果の干渉を解消

--- js/style.js
+++ js/style.js
@@ -10,3 +10,45 @@ $(".nav__btn").on("click ", function(){
   }
   $("+ .nav__sublist > .nav__subitem", this).toggleClass("nav__subitem--active");
 });
+
+let hover = () => {
+    let ua = navigator.userAgent.toLowerCase();
+    $(window).on('load resize orientationchange', function() {
+        if (ua.indexOf('iphone') > 0 || ua.indexOf('ipad') > 0 || ua.indexOf('android') > 0 || ua.indexOf('ipod') > 0 || ua.indexOf('android') > 0 && ua.indexOf('mobile') > 0) {
+            $(".nav__item , .nav__subitem , .nav__sublink").removeClass('hover');
+            console.log("sp");
+        } else {
+            $(".nav__item , .nav__subitem , .nav__sublink").addClass('hover');
+            console.log("pc");
+        }
+    });
+}
+
+$(function(){
+    hover();
+});
+
+
+
+
+
+
+
+
+
+
+
+// $(function () {
+//     var ua = navigator.userAgent.toLowerCase();
+//     if (ua.indexOf('iphone') > 0 || ua.indexOf('ipad') > 0 || ua.indexOf('android') > 0 || ua.indexOf('ipod') > 0 || ua.indexOf('android') > 0 && ua.indexOf('mobile') > 0) {
+//         // スマートフォンの場合実行
+//
+//     } else if (ua.indexOf('ipad') > 0 || ua.indexOf('android') > 0) {
+//         // タブレットの場合実行
+//
+//     } else {
+//         // PC用の場合実行
+//
+//     }
+// })
+// コード選択コード選択
